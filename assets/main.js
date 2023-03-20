4e149305a00e7797a45d3beac0d754f1de80f288
hasan2205 <saifulhasan.dev@gmail.com>
2023-03-20 10:25:10 +0600

    Update

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,19 @@
-console.log("This is javascript program");
+// console.log("This is javascript program");
 
 console.log(26);
+
+var i;
+
+// for(i=0;i<=10;i=i+1){
+//     console.log(i);
+// };
+
+// var f;
+// for(f=1;f<=100;f=f+2){
+//     console.log(f);
+// }
+
+for(i=1;i<=10;i=i+1){
+    var row = "7 * "+ i + " = " + 7 * i;
+    console.log(row);
+};
